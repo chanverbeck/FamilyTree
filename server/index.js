7b8042a0f0863c15ba450b28a27516c98124581f
Channing Verbeck <c_verbeck@hotmail.com>
2015-07-27 14:35:08 -0700

    Return JSON, not readable text.

--- server/index.js
+++ server/index.js
@@ -30,21 +30,25 @@ app.get('/bla', function(request, response) {
 app.get('/person/:personId', function(request, response) {
     var personId = request.params.personId;
     console.log('/person/' + request.params.personId);
-    var res = '';
+    var res = {};
     var husband;
     var wife;
+    var familyId;
     client.query('SELECT * FROM people WHERE pid = $1;', [request.params.personId]).on('row', function(row) {
       console.log('pid = ' + row.pid);
       console.log('name = ' + row.name);
-      res += 'person (' + row.pid + '): ' + row.name;
+      console.log('stringified: ' + JSON.stringify(row));
+      res.person = row;
     });
-    client.query('SELECT husband FROM families WHERE wife = $1', [personId]).on('row', function(row) {
-        console.log ('husband: ' + row.husband);
+    client.query('SELECT * FROM families WHERE wife = $1;', [personId]).on('row', function(row) {
+        console.log ('husband: ' + JSON.stringify(row));
         husband = row.husband;
+        familyId = row.fid;
     });
-    client.query('SELECT wife FROM families WHERE husband = $1', [personId]).on('row', function(row) {
-        console.log ('wife: ' + row.wife);
+    client.query('SELECT * FROM families WHERE husband = $1;', [personId]).on('row', function(row) {
+        console.log ('wife: ' + JSON.stringify(row));
         wife = row.wife;
+        familyId = row.fid;
     }).on('end', function() {
         console.log ('find husband or wife');
         var spouse;
@@ -56,14 +60,22 @@ app.get('/person/:personId', function(request, response) {
         }
         if (spouse) {
             console.log('found spouse: ' + spouse);
-            res += ' married to (' + spouse + ') ';
             client.query('SELECT * FROM people WHERE pid = $1;', [spouse]).on('row', function(row) {
-                res += row.name;
-            }).on('end', function() {
-                response.send(res);
+                res.spouse = row;
+            });
+        }
+        if (familyId) {
+            client.query('SELECT people.* FROM people, children WHERE people.pid = children.pid AND children.fid = $1;', [familyId]).on('row', function(row, result) {
+                console.log('found child' + row.pid);
+                result.addRow(row);
+            }).on('end', function(result) {
+                var childrenJson = JSON.stringify(result.rows);
+                console.log('children: ' + childrenJson);
+                res.children = result.rows;
+                response.send(JSON.stringify(res));
             });
         } else {
-            response.send(res);
+            response.send(JSON.stringify(res));
         }
     });
 });
